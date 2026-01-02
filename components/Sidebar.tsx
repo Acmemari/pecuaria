04bec0990364a9483623e8932f2c6ef0f7571b1b
Acmemari <acmemari@gmail.com>
2026-01-02 08:44:41 -0300

    refactor: simplificar design da bandeira do Brasil no sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -73,7 +73,7 @@ const Sidebar: React.FC<SidebarProps> = ({ agents, activeAgentId, onSelectAgent,
               <rect width="20" height="14" fill="#009739"/>
               <path d="M10 0L20 7L10 14L0 7Z" fill="#FEDD00"/>
               <circle cx="10" cy="7" r="4.5" fill="#012169"/>
-              <path d="M10 4L12 7L10 10L8 7Z" fill="#FFFFFF"/>
+              <path d="M10 5L11.5 7L10 9L8.5 7Z" fill="#FFFFFF"/>
             </svg>
             {/* Bandeira do Paraguai */}
             <svg width="20" height="14" viewBox="0 0 20 14" className="flex-shrink-0" xmlns="http://www.w3.org/2000/svg">
